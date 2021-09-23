90d3f06d98b0538a96b3b20592f85595e48b9239
João Santos <joao.santos@uniksystem.com>
2021-09-23 21:45:16 +0100

    added react routing. created Skills, Home, and Education components in pages, as well as each stylings

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Home from "./Home";
+import Skills from "./Skills";
+import Experience from "./Experience";
+import Education from "./Education";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="main-wrapper default">
-      <Home />
-    </div>
+    <Router>
+      <div className="main-wrapper default">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/education" component={Education} />
+          
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/pages/Skills.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/scss/_skills.scss";
+
+const Skills = () => {
+  return <div>Skills</div>;
+};
+
+export default Skills;
