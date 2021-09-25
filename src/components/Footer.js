d8568d41c32127b3001da8944112b424210ebbd1
João Santos <joao.santos@uniksystem.com>
2021-09-25 16:43:26 +0100

    added footer icons: github, linked, and email with styles. changed styles in header links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "../styles/scss/_footer.scss";
+import { ReactComponent as GithubIcon } from "../styles/img/github.svg";
+import { ReactComponent as Linkedin } from "../styles/img/linkedin.svg";
+import { ReactComponent as Email } from "../styles/img/email.svg";
 
 const Footer = () => {
   return (
     <div className="footer-wrapper">
-      <div className="footer-link">Github</div>
-      <div className="footer-link">LinkedIn</div>
-      <div className="footer-link">Message Me</div>
+      <div className="footer-link">
+        <GithubIcon className="footer-github-icon" />
+      </div>
+      <div className="footer-link">
+        <Linkedin className="footer-linkedin-icon" />
+      </div>
+      <div className="footer-link">
+        <Email className="footer-email-icon" />
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="nav-wrapper">
-      <NavLink className="nav-link" to="/about">About</NavLink>
-      <NavLink className="nav-link" to="/skills">Skills</NavLink>
-      <NavLink className="nav-link" to="/Experience">Experience</NavLink>
-      <NavLink className="nav-link" to="/Education">Education</NavLink>
+      <NavLink activeClassName="nav-selected" className="nav-link" exact to="/">About</NavLink>
+      <NavLink activeClassName="nav-selected" className="nav-link" to="/skills">Skills</NavLink>
+      <NavLink activeClassName="nav-selected" className="nav-link" to="/Experience">Experience</NavLink>
+      <NavLink activeClassName="nav-selected" className="nav-link" to="/Education">Education</NavLink>
     </div>
   );
 };
